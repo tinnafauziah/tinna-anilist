ff686c75b61304d72f7f1f3f5ad0f8f309cbbf0d
tinnafauziah <tinnafauziah@gmail.com>
2022-12-22 06:47:33 +0700

    add animecard

--- components/AnimeCard.vue
+++ components/AnimeCard.vue
@@ -3,13 +3,12 @@ v-card.mb-4.mr-4.anime_card(width='350', @click='redirectAnimeDetail()')
   v-img(height='400', :src='anime.coverImage.large')
   v-card-text
     .subtitle-1.font-weight-bold.mb-1 {{ anime.title | animeTitle }}
-    v-layout(wrap, align-center)
+    v-layout.mb-4(wrap, align-center)
       v-flex.xs1.mr-1
         v-icon(:color='getRatingColor(anime.averageScore)') {{ getRatingIcon(anime.averageScore) }}
       v-flex.xs10.mb-1
         span.subtitle-1 {{ anime.averageScore | animeAverageScore }}
-      v-flex.xs10
-        v-chip(v-for='genre in anime.genres', :key="genre") {{ genre }}
+    v-chip.mb-4.mr-4(v-for='genre in anime.genres', :key="genre") {{ genre }}
 </template>
 
 <script>
